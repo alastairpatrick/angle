51943c066799f885c7468a16eb88837b3637765f
Al <alastairpatrick@gmail.com>
2020-05-20 07:09:13 -0700

    Simplify JavaSmokeTest Add a new build configuration

--- src/tests/easy/JavaSmokeTest.java
+++ src/tests/easy/JavaSmokeTest.java
@@ -2,13 +2,14 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.IntBuffer;
 
-import android.opengl.EGL14;
-import android.opengl.EGL15;
 import android.opengl.EGLConfig;
 import android.opengl.EGLContext;
 import android.opengl.EGLDisplay;
 import android.opengl.EGLSurface;
-import android.opengl.GLES20;
+
+import static android.opengl.EGL14.*;
+import static android.opengl.EGL15.*;
+import static android.opengl.GLES20.*;
 
 public class JavaSmokeTest {
   public static void main(String[] args) {
@@ -20,70 +21,71 @@ public class JavaSmokeTest {
     } catch (InterruptedException e) {
     }*/
 
-    EGLDisplay display = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
+    EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
 
-    int[] major = new int[1];
-    int[] minor = new int[1];
-    if (!EGL14.eglInitialize(display, major, 0, minor, 0)) {
+    if (!eglInitialize(display, null, 0, null, 0)) {
       System.out.format("eglInitialize failed\n");
       System.exit(1);
     }
 
-    System.out.format("Initialized version %d.%d\n", major[0], minor[0]);
-
     int[] configAttribs = new int[] {
-      EGL14.EGL_COLOR_BUFFER_TYPE, EGL14.EGL_RGB_BUFFER,
-      EGL14.EGL_RED_SIZE, 8,
-      EGL14.EGL_GREEN_SIZE, 8,
-      EGL14.EGL_BLUE_SIZE, 8,
-      EGL14.EGL_ALPHA_SIZE, 8,
-      EGL14.EGL_CONFORMANT, EGL14.EGL_OPENGL_ES2_BIT,
-      EGL14.EGL_SURFACE_TYPE, EGL14.EGL_PBUFFER_BIT,
-      EGL14.EGL_NONE,
+      EGL_COLOR_BUFFER_TYPE, EGL_RGB_BUFFER,
+      EGL_RED_SIZE, 8,
+      EGL_GREEN_SIZE, 8,
+      EGL_BLUE_SIZE, 8,
+      EGL_ALPHA_SIZE, 8,
+      EGL_CONFORMANT, EGL_OPENGL_ES2_BIT,
+      EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+      EGL_NONE,
     };
 
     EGLConfig[] configs = new EGLConfig[1];
     int[] numConfigs = new int[1];
-    if (!EGL14.eglChooseConfig(display, configAttribs, 0, configs, 0, 1, numConfigs, 0)) {
+    if (!eglChooseConfig(display, configAttribs, 0, configs, 0, 1, numConfigs, 0)) {
       System.out.format("eglChooseConfig failed\n");
       System.exit(1);
     }
 
+    if (numConfigs[0] < 1) {
+      System.out.format("No EGL configs");
+      System.exit(1);
+    }
+
     int[] surfaceAttribs = new int[] {
-      EGL14.EGL_WIDTH, width,
-      EGL14.EGL_HEIGHT, height,
-      EGL14.EGL_NONE,
+      EGL_WIDTH, width,
+      EGL_HEIGHT, height,
+      EGL_NONE,
     };
 
-    EGLSurface surface = EGL14.eglCreatePbufferSurface(display, configs[0], surfaceAttribs, 0);
+    EGLSurface surface = eglCreatePbufferSurface(display, configs[0], surfaceAttribs, 0);
     if (surface == null) {
       System.out.format("eglCreatePbufferSurface failed\n");
       System.exit(1);
     }
 
     int[] contextAttribs = new int[] {
-      EGL15.EGL_CONTEXT_MAJOR_VERSION, 2,
-      EGL15.EGL_CONTEXT_MINOR_VERSION, 0,
-      EGL14.EGL_NONE,
+      EGL_CONTEXT_MAJOR_VERSION, 2,
+      EGL_CONTEXT_MINOR_VERSION, 0,
+      EGL_NONE,
     };
 
-    EGLContext context = EGL14.eglCreateContext(display, configs[0], null, contextAttribs, 0);
+    EGLContext context = eglCreateContext(display, configs[0], null, contextAttribs, 0);
     if (context == null) {
       System.out.format("eglCreateContext failed\n");
       System.exit(1);
     }
 
-    if (!EGL14.eglMakeCurrent(display, surface, surface, context)) {
+    if (!eglMakeCurrent(display, surface, surface, context)) {
       System.out.format("eglMakeCurrent failed\n");
       System.exit(1);
     }
 
-    GLES20.glClearColor(1.0f, 0.0f, 1.0f, 0.0f);
-    GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    glClearColor(1.0f, 0.0f, 1.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
     ByteBuffer pixels = ByteBuffer.allocateDirect(width * height * 4);
     pixels.order(ByteOrder.nativeOrder());
-    GLES20.glReadPixels(0, 0, width, height, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, pixels);
+    glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 
     IntBuffer pixels32 = pixels.asIntBuffer();
     for (int i = 0; i < width * height; ++i) {
@@ -94,8 +96,10 @@ public class JavaSmokeTest {
       }
     }
 
-    EGL14.eglDestroyContext(display, context);
-    EGL14.eglDestroySurface(display, surface);
-    EGL14.eglTerminate(display);
+    eglDestroyContext(display, context);
+    eglDestroySurface(display, surface);
+    eglTerminate(display);
+
+    System.out.format("Test passed\n");
   }
 }
