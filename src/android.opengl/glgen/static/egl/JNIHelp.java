d26a9942a73784cdb76a722c78dbebe1c68d774f
Al <alastairpatrick@gmail.com>
2020-06-03 08:31:04 -0700

    Load native shared library from jar file is found

--- src/android.opengl/glgen/static/egl/JNIHelp.java
+++ src/android.opengl/glgen/static/egl/JNIHelp.java
@@ -1,5 +1,7 @@
 package android.opengl;
 
+import java.io.InputStream;
+import java.io.IOException;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
@@ -8,6 +10,11 @@ import java.nio.DoubleBuffer;
 import java.nio.IntBuffer;
 import java.nio.LongBuffer;
 import java.nio.ShortBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class JNIHelp {
   public static Object getBufferFields(Buffer buffer, int[] result) {
@@ -44,8 +51,80 @@ public class JNIHelp {
     }
   }
 
+  public static void loadNativeLibraryFromJar(String name) {
+    boolean canDeleteOpen = true;
+    String osName = System.getProperty("os.name").toLowerCase();
+    if (osName.startsWith("windows")) {
+      osName = "windows";
+      canDeleteOpen = false;
+    } else if (osName.startsWith("mac")) {
+      osName = "osx";
+    } else if (osName.startsWith("linux")) {
+      osName = "linux";
+    } else {
+      throw new UnsatisfiedLinkError("JANGLE does not support operating system '" + osName + "'");
+    }
+
+    String arch = System.getProperty("os.arch").toLowerCase();
+    if (arch.startsWith("amd64")) {
+      arch = "x64";
+    } else {
+      throw new UnsatisfiedLinkError("JANGLE does not support processor architecture '" + arch + "'");
+    }
+
+    String filename = System.mapLibraryName(name);
+    String resourcePath = "/android/opengl/" + arch + "-" + osName + "/" + filename;
+    InputStream inStream = JNIHelp.class.getResourceAsStream(resourcePath);
+    if (inStream == null) {
+      throw new UnsatisfiedLinkError("Could not find resource '" + resourcePath + "'");
+    }
+
+    try {
+      Path tempDir = Files.createTempDirectory(null);
+      try {
+        Path tempFile = tempDir.resolve(filename);
+        Files.copy(inStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
+        try {
+          System.load(tempFile.toAbsolutePath().toString());
+        } finally {
+          if (canDeleteOpen) {
+            Files.delete(tempFile);
+          } else {
+            tempFile.toFile().deleteOnExit();
+          }
+        }
+      } finally {
+        if (canDeleteOpen) {
+          Files.delete(tempDir);
+        } else {
+          tempDir.toFile().deleteOnExit();
+        }
+      }
+    } catch (IOException e) {
+      throw new UnsatisfiedLinkError("IO error copying resource '" + resourcePath + "': " + e.getMessage());
+    }
+  }
+
+  private static final AtomicBoolean loadedNativeLibrary = new AtomicBoolean(false);
   public static void loadNativeLibrary() {
-    System.loadLibrary("JANGLE");
+    synchronized (loadedNativeLibrary) {
+      if (loadedNativeLibrary.get()) {
+        return;
+      }
+
+      String libraryName = "JANGLE";
+      try {
+        loadNativeLibraryFromJar(libraryName);
+      } catch (UnsatisfiedLinkError outer) {
+        try {
+          System.loadLibrary(libraryName);
+        } catch (UnsatisfiedLinkError inner) {
+          throw outer;
+        }
+      }
+
+      loadedNativeLibrary.set(true);
+    }
   }
   
   public static native int registerEGL14();
